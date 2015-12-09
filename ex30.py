079aa68dba20b17ac03726bb27c1eabfb1447201
anandarajm <anand.thunder@gmail.com>
2015-12-09 08:40:25 -0800

    ex30 checkin, program about if and elif

---
+++ ex30.py
@@ -0,0 +1,22 @@
+people = 30
+cars = 200
+trucks = 5
+
+if cars>people:
+    print "We have more cars"
+elif cars<people:
+    print "cars are less than number of people"
+else:
+    print "Looks like we have equal number of cars and people"
+
+if trucks> cars:
+    print "Too many trucks :O"
+elif trucks< cars:
+    print "less trucks more cars"
+else:
+    print "looks like we equal cars and trucks"
+
+if people>trucks:
+    print "more people less trucks"
+else:
+    print "Lot of trucks!"
