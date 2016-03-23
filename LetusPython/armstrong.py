f5ed98fcd39d4e2b6f241cdb383d8b694057187a
anandarajm <anand.thunder@gmail.com>
2016-03-23 09:49:11 -0700

    Updated script with optparse instead of argv

--- LetusPython/armstrong.py
+++ LetusPython/armstrong.py
@@ -1,4 +1,5 @@
 import traceback
+from optparse import OptionParser
 from sys import argv
 
 def separate_num(num,digit):
@@ -11,25 +12,40 @@ def separate_num(num,digit):
     return num_list
 
 def armstrong(num,num_list,length):
-    arm = 0
-    for i in num_list:
-        arm += pow(i,length)
-    if num == arm:
-        return True
-    else:
+    if length == 1:
         return False
+    else:
+        arm = 0
+        for i in num_list:
+            arm += pow(i,length)
+        if num == arm:
+            return True
+        else:
+            return False
 
-script , start, stop = argv
+#script , start, stop = argv
 
+
+
+usage = "usage: %prog -b <int> -e <int>"
+parser = OptionParser(usage=usage)
+parser.add_option("-b", dest="begin",
+                  help="Starting range to find Armstrong number", type="int")
+parser.add_option("-e", dest="end",
+                  help="Ending range to find Armstrong number", type="int")
+
+(options, args) = parser.parse_args()
+print "----" %args
+'''
 try:
     start,stop = [int(start),int(stop)]
 except:
     print traceback.print_exc()
     print "Integers only"
     exit()
-
+'''
 num_range = []
-num_range.extend(range(start,(stop+1)))
+num_range.extend(range(options.begin,(options.end+1)))
 
 
 print "-"*80
