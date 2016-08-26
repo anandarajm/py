e4ac4cccec5c200690b5400e63ff5ce2e445a7aa
Anandraj <anand.thunder@gmail.com>
2016-08-25 23:31:38 -0700

    using urllib2 with headers

--- courseraAWD/getwebdata.py
+++ courseraAWD/getwebdata.py
@@ -8,10 +8,19 @@ import urllib2 as ul2
 # sock1.connect(('www.py4inf.com', 80))
 # sock1.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
 
-sock1 = ul2.urlopen('http://www.py4inf.com/code/romeo.txt')
 
-data = sock1.read()
-header = sock1.info()
-print data, header
+site='http://www.pythonlearn.com/code/intro-short.txt'
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
-sock1.close()
+req = ul2.Request(site,headers=hdr)
+page = ul2.urlopen(req)
+
+print page.info()
+print page.read()
+
+
