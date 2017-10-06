fd6b412e35aade88654b007470968043e543c90b
Anandraj <anand.thunder@gmail.com>
2017-10-06 13:08:28 -0700

    complete

--- Questions/reverse.py
+++ Questions/reverse.py
@@ -1,15 +1,31 @@
-import string
+def answer(s):
+    import string
+    alphabets = string.lowercase[:]
+    alphalist = []
+    revlist = []
+    for alpha in alphabets:
+        alphalist.append(alpha)
+        revlist.append(alpha)
+    revlist.reverse()
+    sentence = 'xyz'
+    revsent = str()
 
-alphabets = string.lowercase[:]
-alphalist = []
-revlist = []
-for alpha in alphabets:
-    alphalist.append(alpha)
-    revlist.append(alpha)
-revlist.reverse()
-sentence = 'xyz'
-revsent = None
+    for chars in s:
+        if chars == ' ':
+            revsent += ' '
+        else:
+            pos = alphalist.index(chars)
+            char = revlist[pos]
+            revsent += char
+    print revsent
 
-for s in sentence:
-    pos = alphalist.index(s)
-    print revlist[pos]
+
+def main():
+    try:
+        insent = raw_input("Enter string to reverse:")
+    except:
+        pass
+    outsent = answer(insent)
+
+if __name__== "__main__":
+    main()
